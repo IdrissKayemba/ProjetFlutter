6299bc43d3ca97a7ce5d4340f91c27e3dbffbdff
IdrissKayemba <157881025+IdrissKayemba@users.noreply.github.com>
2024-01-29 22:52:17 +0100

    main.dart

--- main.dart
+++ main.dart
@@ -1,118 +1,50 @@
 import 'package:flutter/material.dart';
 
-class Product {
-  const Product({required this.name});
 
-  final String name;
-}
 
-typedef CartChangedCallback = Function(Product product, bool inCart);
-
-class ShoppingListItem extends StatelessWidget {
-  ShoppingListItem({
-    required this.product,
-    required this.inCart,
-    required this.onCartChanged,
-  }) : super(key: ObjectKey(product));
-
-  final Product product;
-  final bool inCart;
-  final CartChangedCallback onCartChanged;
-
-  Color _getColor(BuildContext context) {
-    // The theme depends on the BuildContext because different
-    // parts of the tree can have different themes.
-    // The BuildContext indicates where the build is
-    // taking place and therefore which theme to use.
-
-    return inCart //
-        ? Colors.black54
-        : Theme.of(context).primaryColor;
-  }
-
-  TextStyle? _getTextStyle(BuildContext context) {
-    if (!inCart) return null;
-
-    return const TextStyle(
-      color: Colors.black54,
-      decoration: TextDecoration.lineThrough,
-    );
-  }
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      onTap: () {
-        onCartChanged(product, inCart);
-      },
-      leading: CircleAvatar(
-        backgroundColor: _getColor(context),
-        child: Text(product.name[0]),
-      ),
-      title: Text(
-        product.name,
-        style: _getTextStyle(context),
-      ),
-    );
-  }
-}
-
-class ShoppingList extends StatefulWidget {
-  const ShoppingList({required this.products, super.key});
-
-  final List<Product> products;
-
-
-  @override
-  State<ShoppingList> createState() => _ShoppingListState();
-}
-
-class _ShoppingListState extends State<ShoppingList> {
-  final _shoppingCart = <Product>{};
-
-  void _handleCartChanged(Product product, bool inCart) {
-    setState(() {
-
-
-      if (!inCart) {
-        _shoppingCart.add(product);
-      } else {
-        _shoppingCart.remove(product);
-      }
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Barrez les produits que vous n'achetez pas:"),
-      ),
-      body: ListView(
-        padding: const EdgeInsets.symmetric(vertical: 8),
-        children: widget.products.map((product) {
-          return ShoppingListItem(
-            product: product,
-            inCart: _shoppingCart.contains(product),
-            onCartChanged: _handleCartChanged,
-          );
-        }).toList(),
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.black,
+          title: const Text(
+            "L'aperçu de votre carte",
+            style: TextStyle(
+              color: Colors.white
+            ),),
+        ),
+        body: Center(
+          child: Container(
+            padding: EdgeInsets.all(30),
+            color: Colors.green,
+            child: Text(
+              'Intitulé: Idriss KAYEMBA \n Solde: 90,00 USD \n 1289 2376 4543 2190',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 20,
+                
+              ),
+            ),
+            
+          ),
+        ),
+        floatingActionButton: ElevatedButton(
+          onPressed: null,
+          
+          child: Text('Ajouter une carte',
+          style: TextStyle(
+                color: Colors.black,
+                fontSize: 20,
+              ),
+          ),
+        ),
       ),
     );
   }
 }
 
 void main() {
-  runApp(const MaterialApp(
-    title: 'Idriss Shopping',
-    home: ShoppingList(
-      products: [
-        Product(name: 'Oeufs frais'),
-        Product(name: "Huille d'olive"),
-        Product(name: 'Pain au lait'),
-        Product(name: 'Beure Irlandais'),
-        Product(name: 'Lait Thailandais'),
-      ],
-    ),
-  ));
+  runApp(MyApp());
 }
